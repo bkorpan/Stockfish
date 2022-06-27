23f3f984ab702db3514bdadca5847b8bbd0adb34
Ben Korpan <bkorpan@berkeley.edu>
2022-06-27 10:16:27 -0700

    Added modified qsearch routine for ftbfs

--- src/search.cpp
+++ src/search.cpp
@@ -1790,6 +1790,149 @@ moves_loop: // When in check, search starts here
 
 //=============================== FTBFS stuff ======================================
 
+  template <NodeType nodeType>
+  Value qsearch_ftbs(Position& pos, Value alpha, Value beta, Depth depth, int ply) {
+
+    static_assert(nodeType != Root);
+    constexpr bool PvNode = nodeType == PV;
+
+    assert(alpha >= -VALUE_INFINITE && alpha < beta && beta <= VALUE_INFINITE);
+    assert(PvNode || (alpha == beta - 1));
+    assert(depth <= 0);
+
+    Move pv[MAX_PLY+1];
+    StateInfo st;
+    ASSERT_ALIGNED(&st, Eval::NNUE::CacheLineSize);
+
+    TTEntry* tte;
+    Key posKey;
+    Move ttMove, move, bestMove;
+    Depth ttDepth;
+    Value bestValue, value, ttValue, futilityValue, futilityBase;
+    bool pvHit, givesCheck, capture;
+    int moveCount;
+
+    Thread* thisThread = pos.this_thread();
+    bestMove = MOVE_NONE;
+    moveCount = 0;
+
+    // Check for an immediate draw or maximum ply reached
+    if (   pos.is_draw(ply)
+        || ply >= MAX_PLY)
+        return (ply >= MAX_PLY && !pos.checkers()) ? evaluate(pos) : VALUE_DRAW;
+
+    assert(0 <= ply && ply < MAX_PLY);
+
+    // Decide whether or not to include checks: this fixes also the type of
+    // TT entry depth that we are going to use. Note that in qsearch we use
+    // only two types of depth in TT: DEPTH_QS_CHECKS or DEPTH_QS_NO_CHECKS.
+    ttDepth = pos.checkers() || depth >= DEPTH_QS_CHECKS ? DEPTH_QS_CHECKS
+                                                         : DEPTH_QS_NO_CHECKS;
+
+    // Initialize a MovePicker object for the current position, and prepare
+    // to search the moves. Because the depth is <= 0 here, only captures,
+    // queen promotions, and other checks (only if depth >= DEPTH_QS_CHECKS)
+    // will be generated.
+    Square prevSq = to_sq((ss-1)->currentMove);
+    MovePicker mp(pos, ttMove, depth, &thisThread->mainHistory,
+                                      &thisThread->captureHistory,
+                                      NULL,
+                                      prevSq);
+
+    int quietCheckEvasions = 0;
+
+    // Loop through the moves until no moves remain or a beta cutoff occurs
+    while ((move = mp.next_move()) != MOVE_NONE)
+    {
+      assert(is_ok(move));
+
+      // Check for legality
+      if (!pos.legal(move))
+          continue;
+
+      givesCheck = pos.gives_check(move);
+      capture = pos.capture(move);
+
+      moveCount++;
+
+      // Futility pruning and moveCount pruning (~5 Elo)
+      if (    bestValue > VALUE_TB_LOSS_IN_MAX_PLY
+          && !givesCheck
+          &&  to_sq(move) != prevSq
+          &&  futilityBase > -VALUE_KNOWN_WIN
+          &&  type_of(move) != PROMOTION)
+      {
+
+          if (moveCount > 2)
+              continue;
+
+          futilityValue = futilityBase + PieceValue[EG][pos.piece_on(to_sq(move))];
+
+          if (futilityValue <= alpha)
+          {
+              bestValue = std::max(bestValue, futilityValue);
+              continue;
+          }
+
+          if (futilityBase <= alpha && !pos.see_ge(move, VALUE_ZERO + 1))
+          {
+              bestValue = std::max(bestValue, futilityBase);
+              continue;
+          }
+      }
+
+      // Do not search moves with negative SEE values (~5 Elo)
+      if (    bestValue > VALUE_TB_LOSS_IN_MAX_PLY
+          && !pos.see_ge(move))
+          continue;
+
+      // movecount pruning for quiet check evasions
+      if (   bestValue > VALUE_TB_LOSS_IN_MAX_PLY
+          && quietCheckEvasions > 1
+          && !capture
+          && pos.checkers())
+          continue;
+
+      quietCheckEvasions += !capture && pos.checkers();
+
+      // Make and search the move
+      pos.do_move(move, st, givesCheck);
+      value = -ftbs_qsearch<PV>(pos, -beta, -alpha, depth - 1, ply);
+      pos.undo_move(move);
+
+      assert(value > -VALUE_INFINITE && value < VALUE_INFINITE);
+
+      // Check for a new best move
+      if (value > bestValue)
+      {
+          bestValue = value;
+
+          if (value > alpha)
+          {
+              bestMove = move;
+
+              if (PvNode && value < beta) // Update alpha here!
+                  alpha = value;
+              else
+                  break; // Fail high
+          }
+       }
+    }
+
+    // All legal moves have been searched. A special case: if we're in check
+    // and no legal moves were found, it is checkmate.
+    if (pos.checkers() && bestValue == -VALUE_INFINITE)
+    {
+        assert(!MoveList<LEGAL>(pos).size());
+
+        return mated_in(ply); // Plies to mate from the root
+    }
+
+    assert(bestValue > -VALUE_INFINITE && bestValue < VALUE_INFINITE);
+
+    return bestValue;
+  }
+
 class Node {
 private:
   int num_edges;
@@ -1801,11 +1944,11 @@ private:
   Move* moves;
 
   // Use qsearch to put initial valuations on edges
-  void init_edge_values(Position& pos) {
+  void init_edge_values(Position& pos, int ply) {
     for (int i = 0; i < num_edges; i++) {
       StateInfo st;
       pos.do_move(moves[i], st);
-      values[i] = qsearch<NonPV>(pos, NULL, -VALUE_INFINITE, VALUE_INFINITE);
+      values[i] = qsearch_ftbs<PV>(pos, -VALUE_INFINITE, VALUE_INFINITE, 0, ply);
       pos.undo_move(moves[i]);
     }
   }
@@ -1828,7 +1971,7 @@ private:
 
 public:
 
-  Node(Position &pos, Node* parent) {
+  Node(Position &pos, Node* parent, int ply) {
     // Generate legal moves from this node
     ExtMove moves_array[121];
     ExtMove* moves_start = moves_array;
@@ -1845,7 +1988,7 @@ public:
 
     // Initialize arrays
     memset(edges, 0, num_edges * sizeof(Node*));
-    init_edge_values(pos);
+    init_edge_values(pos, ply);
     for (int i = 0; i < num_edges; i++) {
       moves[i] = moves_array[i].move;
     }
@@ -1866,10 +2009,10 @@ public:
     return moves[get_best_idx()];
   }
 
-  Node* expand_best(Position &pos) {
+  Node* expand_best(Position &pos, int ply) {
     StateInfo st;
     pos.do_move(moves[get_best_idx()], st);
-    return new Node(pos, this);
+    return new Node(pos, this, ply);
   }
 
   Value get_value() {
@@ -1909,7 +2052,7 @@ void swap_and_negate(Value &a, Value &b) {
 
 Value ftbfs(Position& pos, int n) {
   // Root node
-  Node* root = new Node(pos, NULL);
+  Node* root = new Node(pos, NULL, 0);
   Node* node = root;
 
   // Initialize values
@@ -1923,7 +2066,7 @@ Value ftbfs(Position& pos, int n) {
   // Search loop
   for (int i = 0; i < n; i++) {
     // Expand best move of current best node
-    node = node->expand_best(pos);
+    node = node->expand_best(pos, d);
     value = node->get_value();
     d++;
 
